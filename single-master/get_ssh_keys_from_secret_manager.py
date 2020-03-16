f0d93b8258924c1b5c4bf5629340121d1bfc4f1b
Fabio Ponciroli <ponch78@gmail.com>
2020-03-16 11:45:52 +0100

    Persist Gerrit volumes
    Make sure Gerrit volumes are persisted if the container dies
    Feature: Issue 12465
    
    Change-Id: I46469b50bd6b2d73978c417ef32b2086a90f6cc3

---
+++ single-master/get_ssh_keys_from_secret_manager.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import boto3
+import base64
+from botocore.exceptions import ClientError
+
+def get_secret(secret_name):
+
+    region_name = "us-east-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+    else:
+        # Decrypts secret using the associated KMS CMK.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if 'SecretString' in get_secret_value_response:
+            return get_secret_value_response['SecretString']
+        else:
+            return base64.b64decode(get_secret_value_response['SecretBinary'])
+
+secretIds = [
+    "ssh_host_ecdsa_384_key",
+    "ssh_host_ecdsa_384_key.pub",
+    "ssh_host_ecdsa_521_key",
+    "ssh_host_ecdsa_521_key.pub",
+    "ssh_host_ecdsa_key",
+    "ssh_host_ecdsa_key.pub",
+    "ssh_host_ed25519_key",
+    "ssh_host_ed25519_key.pub",
+    "ssh_host_rsa_key",
+    "ssh_host_rsa_key.pub"
+]
+
+GERRIT_KEY_PREFIX = "gerrit_secret_"
+GERRIT_SSH_KEYS_DIRECTORY = "/var/gerrit/etc/"
+
+print("Installing SSH Keys from Secret Manager in directory: " + GERRIT_SSH_KEYS_DIRECTORY)
+for secretId in secretIds:
+    print("* Installing SSH Key: " + secretId)
+    with open(GERRIT_SSH_KEYS_DIRECTORY + secretId, 'w', encoding = 'utf-8') as f:
+        f.write(get_secret(GERRIT_KEY_PREFIX + secretId))
