7f9a56dd3fd7b0679c0295bb7f7b0bc51ba93e16
Fabio Ponciroli <ponch78@gmail.com>
2020-03-31 19:43:19 +0200

    AWS Gerrit master-slave recipe
    CF templates and scripts to set up a Gerrit master
    instance replicating to a slave
    
    Feature: Issue 12516
    Change-Id: I46aa690e9fc2435d29259003eed47d672bb73d29

--- gerrit/entrypoint.sh
+++ gerrit/entrypoint.sh
@@ -9,12 +9,22 @@ then
   git config -f /var/gerrit/etc/gerrit.config --add container.javaOptions "-Djava.security.egd=file:/dev/./urandom"
 fi
 
-git config -f /var/gerrit/etc/gerrit.config gerrit.canonicalWebUrl "${CANONICAL_WEB_URL:-http://$HOSTNAME}"
-git config -f /var/gerrit/etc/gerrit.config httpd.listenUrl "${HTTPD_LISTEN_URL:-http://*:8080/}"
+if [ $CONTAINER_SLAVE ]
+then
+  echo "Waiting 3 minutes for replication from master to happen before starting slave"
+  sleep 180
+else
+  echo "Master instance, no need to sleep"
+fi
 
 if [ "$1" != "init" ]
 then
   /tmp/setup_gerrit.py
+
+  git config -f /var/gerrit/etc/gerrit.config gerrit.canonicalWebUrl "${CANONICAL_WEB_URL:-http://$HOSTNAME}"
+  git config -f /var/gerrit/etc/gerrit.config httpd.listenUrl "${HTTPD_LISTEN_URL:-http://*:8080/}"
+  git config -f /var/gerrit/etc/gerrit.config container.slave "${CONTAINER_SLAVE:-false}"
+
   echo "Running Gerrit ..."
   exec /var/gerrit/bin/gerrit.sh run
 fi
